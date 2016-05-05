3906990171893a40f67e606e5c398f8c57a2ad65
Djibril <djibril.dieng@isen-lille.fr>
2016-05-05 15:42:21 +0200

    Commit 1 ---> SalesTool.

---
+++ SalesTool/src/ca/demo/salestool/Main.java
@@ -0,0 +1,16 @@
+package ca.demo.salestool;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		SalesData data = new SalesData();
+		data.display();
+	}
+	
+	public static void displayGreeting(){
+		System.out.println("HELLO HAPY SALES PEOPLE!");
+		System.out.println("THIS APP SHOW SALE DATA");
+	}
+
+}
